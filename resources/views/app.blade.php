1d87aacd6329e56f407b95592c1b67f9fc6ae594
jemarple1 <99503444+jemarple1@users.noreply.github.com>
2022-11-07 21:52:21 -0500

    font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,13 +7,19 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
+    <style>
+            body {
+                font-family: 'Source Sans Pro', sans-serif!important;
+            }
+        </style>
     <body class="font-sans antialiased">
         @inertia
     </body>
