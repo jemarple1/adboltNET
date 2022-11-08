0a68920f96291976de797a027d6a1660f4c1e651
jemarple1 <99503444+jemarple1@users.noreply.github.com>
2022-11-07 21:56:21 -0500

    goog analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,16 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
         <!-- Scripts -->
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-54754154-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-54754154-1');
+</script>
+
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
